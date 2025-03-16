4c73f4cc6ddc615776fa7c0f7b907dbbc93fdc28
eru_roraito <eru.lawliet.death@gmail.com>
2025-03-17 04:45:54 +0700

    Update

---
+++ src/main/menu_toggle.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-toggle").addEventListener("click", function () {
+    document.querySelector(".nav-list").classList.toggle("active");
+    document.getElementById("header").classList.toggle("column");
+});
